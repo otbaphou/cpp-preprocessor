c2c008f2d6ccba6811a03e362c86974ab30fb932
0tba <72134530+otbaphou@users.noreply.github.com>
2024-01-10 13:00:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,32 +14,31 @@ path operator""_p(const char* data, std::size_t sz) {
     return path(data, data + sz);
 }
 
-vector<string> UnpackFile(path& in_file, const vector<path>& include_directories, size_t lineNum)
+pair<vector<string>, bool> UnpackFile(const path& in_file, const vector<path>& include_directories, size_t line_num)
 {
     static regex include_reg_f(R"(\s*#\s*include\s*"([^"]*)\"\s*)");
     static regex include_reg_s(R"(\s*#\s*include\s*<([^>]*)>\s*)");
     
     vector<string> rows;
     
-    ifstream inFile(in_file);
+    ifstream in_file_unpack(in_file);
     
-    if(!inFile.is_open())
-        return {};
+    if(!in_file_unpack.is_open())
+        return {{}, false};
     
     string line;
-    while(getline(inFile, line))
+    while(getline(in_file_unpack, line))
     {
         smatch match;
         if(regex_match(line, match, include_reg_f))
         {
-            path p = in_file.parent_path() / string(match[1]);
+            path file_path = in_file.parent_path() / string(match[1]);
             
-            vector<string> lines = UnpackFile(p, include_directories, lineNum);
+            pair<vector<string>, bool> lines = UnpackFile(file_path, include_directories, line_num);
             
-            if(!lines.empty())
+            if(!lines.first.empty())
             {
-
-            for(string& line_ : lines)
+            for(string& line_ : lines.first)
                 rows.push_back(line_);
             }else
             {
@@ -50,35 +49,43 @@ vector<string> UnpackFile(path& in_file, const vector<path>& include_directories
                     path tmp = dir / string(match[1]);
                     if(filesystem::exists(tmp))
                     {
-                        ifstream includeFile(tmp);
-
-                        for(string& line_ : UnpackFile(tmp, include_directories, lineNum))
-                    rows.push_back(line_);
+                        ifstream include_file(tmp);
+                        
+                        if(!include_file.is_open())
+                        { 
+                        cout << "unknown include file " << tmp.filename().string()
+                        << " at file " << in_file.string() << " at line " << line_num << endl;
+                        return {rows, false};
+                        }
+                        
+                        for(string& line_ : UnpackFile(tmp, include_directories, line_num).first)
+                        rows.push_back(line_);
                     
-                        found=true;
+                        found = true;
                         break;
                     }
                 }
                 
                     if (!found) 
                     {
-                    cout << "unknown include file " << p.filename().string() << " at file " << in_file.string() << " at line " << lineNum << endl;
-                    return rows;
+                    cout << "unknown include file " << file_path.filename().string()
+                    << " at file " << in_file.string() << " at line " << line_num << endl;
+                    return {rows, false};
                     }
             }
         }else if(regex_match(line, match, include_reg_s))
         {
             bool found = false;
-            path p = string(match[1]);
+            path file_path = string(match[1]);
             
-            for(auto& dir : include_directories)
+            for(const auto& dir : include_directories)
             {
-                path tmp = dir / p;
+                path tmp = dir / file_path;
                 if(filesystem::exists(tmp))
                 {
-                    ifstream includeFile(tmp);
+                    ifstream include_file(tmp);
 
-                    for(string& line_ : UnpackFile(tmp, include_directories, lineNum))
+                    for(string& line_ : UnpackFile(tmp, include_directories, line_num).first)
                 rows.push_back(line_);
                     
                     found=true;
@@ -86,14 +93,18 @@ vector<string> UnpackFile(path& in_file, const vector<path>& include_directories
                 }
                 if (!found) 
                 {
-                cout << "unknown include file " << p.filename().string() << " at file " << in_file.string() << " at line " << lineNum << endl;
-                return rows;
+                cout << "unknown include file " << file_path.filename().string()
+                << " at file " << in_file.string() << " at line " << line_num << endl;
+                return {rows, false};
                 }
             }
-        }else
+        } else
+        {
            rows.push_back(line); 
+        }
+        line_num++;
     }
-    return rows;
+    return {rows, true};
 }
                  
 
@@ -104,96 +115,23 @@ bool Preprocess(const path& in_file, const path& out_file, const vector<path>& i
     static regex include_reg_s(R"(\s*#\s*include\s*<([^>]*)>\s*)");
     const path pPath = in_file.parent_path();
     
-    ifstream inFile(in_file);
-    if(!inFile.is_open())
+    ifstream in_file_preprocess_(in_file);
+    if(!in_file_preprocess_.is_open())
+    {
         return false;
-    ofstream outFile(out_file);
+    }
+    ofstream out_file_preprocess_(out_file);
     
     string line;
-    size_t lineNum = 1;
-    while(getline(inFile, line))
+    pair<vector<string>, bool> lines = UnpackFile(in_file, include_directories, 1);
+    
+    for(string& line_ : lines.first)
     {
-        smatch match;
-        if(regex_match(line, match, include_reg_f))
-        {        
-            path p = string(match[1]);
-            path includePath = in_file.parent_path() / p;
-            
-            bool found = false;
-            
-            if(filesystem::exists(includePath))
-                found = true;
-            else
-            {
-                for(auto& dir : include_directories)
-                {
-                    path tmp = dir / p;
-                    if(filesystem::exists(tmp))
-                    {
-                        includePath = tmp;
-                        found=true;
-                        break;
-                    }
-                }
-            }
-            
-            if(!found)
-            { 
-                cout << "unknown include file " << p.filename().string() << " at file " << in_file.string() << " at line " << lineNum << endl;
-                return false;
-            }
-
-            ifstream includeFile(includePath);
-            if(!includeFile.is_open())
-            { 
-                cout << "unknown include file " << p.filename().string() << " at file " << in_file.string() << " at line " << lineNum << endl;
-                return false;
-            }
-            
-            for(string& line_ : UnpackFile(includePath, include_directories, lineNum))
-            {
-                outFile << line_ << '\n';
-            }
-            
-        }else if(regex_match(line, match, include_reg_s))
-        {
-
-            path p = string(match[1]);
-            bool found = false;
-            
-            for(auto& dir : include_directories)
-            {
-                path tmp = dir / p;
-                if(filesystem::exists(tmp))
-                {
-                    p = tmp;
-                    found=true;
-                    break;
-                }
-
-            }
-                if (!found) 
-                {
-                cout << "unknown include file " << p.filename().string() << " at file " << in_file.string() << " at line " << lineNum << endl;
-                return false;
-                }
-            vector<string> lines = UnpackFile(p, include_directories, lineNum);
-            for(string& line_ : lines)
-            {
-                outFile << line_ << '\n';
-            }
-            if (lines.empty()) 
-            {
-            cout << "unknown include file " << p.filename().string() << " at file " << in_file.string() << " at line " << lineNum << endl;
-            return false;
-            }
-            
-        }else
-        {
-            outFile << line << endl;
-        }
-        lineNum++;
+        out_file_preprocess_ << line_ << '\n';
     }
+    
+    if(!lines.second)
+        return false;
     
     return true;
 }
@@ -249,11 +187,7 @@ void Test() {
         file << "// std2\n"s;
     }
 
-    //assert((!Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p, {"sources"_p / "include1"_p,"sources"_p / "include2"_p})));
-    
-Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p, {"sources"_p / "include1"_p,"sources"_p / "include2"_p});
-    
-    cout << GetFileContents("a.in");
+    assert((!Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p, {"sources"_p / "include1"_p,"sources"_p / "include2"_p})));
     
     ostringstream test_out;
     test_out << "// this comment before include\n"
@@ -269,8 +203,9 @@ Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p, {"sources"_p / "incl
                 "\n"
                 "int SayHello() {\n"
                 "    cout << \"hello, world!\" << endl;\n"s;
-
-    //assert(GetFileContents("sources/a.in"s) == test_out.str());
+    //cout << GetFileContents("a.in");
+    
+    assert(GetFileContents("sources/a.in"s) == test_out.str());
 }
 
 int main() {
